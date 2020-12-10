a2b8aa80a9aa1d3a0f574b88cc3eedfdeeafe599
ibrahimcanyilmaz <ibrahim.yilmaz@startuphero.es>
2020-12-10 09:50:56 +0300

    Fix 'push only to current branch' function

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,27 +9,13 @@ GIT_USERNAME=${INPUT_GIT_USERNAME:-${GIT_USERNAME:-"git"}}
 REMOTE=${INPUT_REMOTE:-"$*"}
 GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
 GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
-GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
+GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--force --all"}
 GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
 GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
-HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2>/dev/null || /bin/true)"
 
 
-if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
-    echo "WARNING: repo not checked out; attempting checkout" > /dev/stderr
-    echo "WARNING: this may result in missing commits in the remote mirror" > /dev/stderr
-    echo "WARNING: this behavior is deprecated and will be removed in a future release" > /dev/stderr
-    echo "WARNING: to remove this warning add the following to your yml job steps:" > /dev/stderr
-    echo " - uses: actions/checkout@v1" > /dev/stderr
-    if [[ "${SRC_REPO}" -eq "" ]]; then
-        echo "WARNING: SRC_REPO env variable not defined" > /dev/stderr
-        SRC_REPO="https://github.com/${GITHUB_REPOSITORY}.git" > /dev/stderr
-        echo "Assuming source repo is ${SRC_REPO}" > /dev/stderr
-     fi
-    git init > /dev/null
-    git remote add origin "${SRC_REPO}"
-    git fetch --all > /dev/null 2>&1
-fi
+
+
 
 git config --global credential.username "${GIT_USERNAME}"
 
@@ -61,16 +47,6 @@ else
     git config --global credential.helper cache
 fi
 
-
 git remote add mirror "${REMOTE}"
-if [[ "${INPUT_PUSH_ALL_REFS}" != "false" ]]; then
-    eval git push ${GIT_PUSH_ARGS} mirror "\"refs/remotes/origin/*:refs/heads/*\""
-else
-    if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
-        echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch" > /dev/stderr
-        exit 1
-    else
-				eval git config --global push.default current
-        eval git push -u ${GIT_PUSH_ARGS} mirror
-    fi
-fi
+git config --global push.default current
+git push ${GIT_PUSH_ARGS} mirror
